d393357f605345481133cf94fb299ac2aec96a9f
ericaforkbison <134523373+ericaforkbison@users.noreply.github.com>
2025-06-13 00:50:16 -0400

    Spring Boot running

--- db/init/create_tables.sql
+++ db/init/create_tables.sql
@@ -8,18 +8,18 @@ DROP TABLE IF EXISTS color;
 CREATE TABLE bird (
     id INT AUTO_INCREMENT PRIMARY KEY,
     common_name VARCHAR(100) NOT NULL,
-    scientific_name VARCHAR(100) NOT NULL,
-)
+    scientific_name VARCHAR(100) NOT NULL
+);
 
 CREATE TABLE color (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-)
+    name VARCHAR(100) NOT NULL
+);
 
 CREATE TABLE habitat (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-)
+    name VARCHAR(100) NOT NULL
+);
 
 -- many-to-many btwn bird/color
 CREATE TABLE bird_color (
@@ -28,7 +28,7 @@ CREATE TABLE bird_color (
     PRIMARY KEY (bird_id, color_id),
     FOREIGN KEY (bird_id) REFERENCES bird(id),
     FOREIGN KEY (color_id) REFERENCES color(id)
-)
+);
 
 -- many-to-many btwn bird/habitat
 CREATE TABLE bird_habitat (
@@ -37,4 +37,4 @@ CREATE TABLE bird_habitat (
     PRIMARY KEY (bird_id, habitat_id),
     FOREIGN KEY (bird_id) REFERENCES bird(id),
     FOREIGN KEY (habitat_id) REFERENCES habitat(id)
-)
+);
